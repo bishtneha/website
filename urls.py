d1ffae1322a8748cea428a50465f07ac63cbaa21
bishtneha <47822776+bishtneha@users.noreply.github.com>
2019-02-20 09:49:25 -0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,16 +1,25 @@
-from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
+app_name = 'music'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('music/', include('music.urls')),
-    path('', include('music.urls')),
+
+         path('', views.index, name='index'),
+         path('register/', views.register, name='register'),
+         path('login_user/', views.login_user, name='login_user'),
+         path('logout_user/', views.logout_user, name='logout_user'),
+         path('<album_id>/', views.detail, name='detail'),
+         path('<song_id>/favorite/', views.favorite, name='favorite'),
+         path('songs/<filter_by>/', views.songs, name='songs'),
+         path('create_album/', views.create_album, name='create_album'),
+         path('<album_id>/create_song/', views.create_song, name='create_song'),
+         path('<album_id>/delete_song/<song_id>/', views.delete_song, name='delete_song'),
+         path('<album_id>/favorite_album/', views.favorite_album, name='favorite_album'),
+         path('<album_id>/delete_album/', views.delete_album, name='delete_album'),
+
+
+
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
